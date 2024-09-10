433a37b58ad221554d846cf0f8fbb0e64b113307
Alexis Praiz <alexispraiz18@gmail.com>
2024-09-10 15:07:17 -0300

    Agregado de semántica mediante atributos en etiquetas HTML. Además de refactorización de código css en secciones 'features/contact/questions'. Index.js agregado

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/* Abrir - Cerrar Menú Mobile */
+const $menu       = document.querySelector(".nav");
+const $close_menu = document.getElementById("close-menu");
+const $open_menu  = document.getElementById("open-menu");
+
+$open_menu.addEventListener("click",()=>{ $menu.classList.add("open") });
+$close_menu.addEventListener("click",()=>{ $menu.classList.remove("open") });
+
+/* Tabulador de pestañas "feature" */
+const $slider = document.getElementById("slider");
+const $slide  = Array.from($slider.children);
+const $feature_tab = Array.from(document.querySelector(".feature-tab").children);
+
+$slider.addEventListener("click", (e)=>{
+    const tabID = e.target.id;
+
+    $feature_tab.forEach((child)=>{
+        if(child.getAttribute("aria-labelledby") == tabID){
+            child.classList.add("show-feature");
+            child.removeAttribute("hidden");
+        }else{
+            child.classList.remove("show-feature");
+            child.setAttribute("hidden", "");
+        }
+    });
+
+    $slide.forEach((child)=>{ child.id == tabID ? child.setAttribute("aria-selected", "true") & child.classList.add("active-slide") : child.setAttribute("aria-selected", "false") & child.classList.remove("active-slide") });
+});
+
+
+/* Simulación de envío de correo en la sección "Contact" */
